65166ac93eade847609242b2e49b14eb9e823a02
Douglas Silva Santos <doug.neo@gmail.com>
2020-11-09 17:37:57 -0300

    Programa Funcional (ainda faltando o if do deposito inicial), porem ainda falta torna-lo Orientado ao Objeto

---
+++ Abertura de Conta/ContaBancaria.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Abertura_de_Conta
+{
+    class ContaBancaria
+    {
+        public int Numero;
+        public string Titular;
+        public double Saldo;
+
+        public void Deposito(double quantia)
+        {
+            Saldo += quantia;
+        }
+
+        public void Saque(double quantia)
+        {
+            double taxa = 5;
+            Saldo = Saldo - quantia - taxa;
+        }
+
+        public override string ToString()
+        {
+            return "Conta " + Numero + ", Titular: " + Titular + ", Saldo: R$" + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+
+    }
+}
--- Abertura de Conta/Program.cs
+++ Abertura de Conta/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Abertura_de_Conta
 {
@@ -6,7 +7,54 @@ namespace Abertura_de_Conta
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ContaBancaria corrente = new ContaBancaria();
+            
+            Console.Write("Entre o número da conta: ");
+            corrente.Numero = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            Console.Write("Entre o titular da conta: ");
+            corrente.Titular = Console.ReadLine();
+            
+            Console.Write("Haverá depósito inicial(s / n) ? ");
+            char di = char.Parse(Console.ReadLine());
+           
+            Console.Write("Entre o valor de depósito inicial: ");
+            corrente.Saldo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(corrente);
+            Console.WriteLine();
+
+            Console.Write("Entre um valor para deposito: ");
+            double quantia = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            corrente.Deposito(quantia);
+            Console.WriteLine("Dados da conta atualizado");
+            Console.WriteLine(corrente);
+            Console.WriteLine();
+
+            Console.Write("Entre com um valor para saque: ");
+            quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            corrente.Saque(quantia);
+            Console.WriteLine("Dados da conta atualizado");
+            Console.WriteLine(corrente);
         }
     }
 }
+
+
+
+
+
+/*Entre o número da conta: 8532
+Entre o titular da conta: Alex Green
+Haverá depósito inicial (s/n)? s
+Entre o valor de depósito inicial: 500.00
+Dados da conta:
+Conta 8532, Titular: Alex Green, Saldo: $ 500.00
+Entre um valor para depósito: 200.00
+Dados da conta atualizados:
+Conta 8532, Titular: Alex Green, Saldo: $ 700.00
+Entre um valor para saque: 300.00
+Dados da conta atualizados:
+Conta 8532, Titular: Alex Green, Saldo: $ 395.00*/
